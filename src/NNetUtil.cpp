66d64eae0fb4e243a44e1c354e479f3fac1d727a
joelnir <joel.oskarsson@outlook.com>
2018-08-17 17:34:43 +0200

    Add util function to calculate square error

--- src/NNetUtil.cpp
+++ src/NNetUtil.cpp
@@ -1,4 +1,5 @@
 #include <math.h>
+
 #include "NNetUtil.h"
 
 double NNetUtil::unitStep(double x){
@@ -12,4 +13,14 @@ double NNetUtil::unitStep(double x){
 
 double NNetUtil::sigmoid(double x){
     return 1.0 / (1.0 + exp(-1.0 * x));
+}
+
+double NNetUtil::squareError(std::vector<double>& v1, std::vector<double>& v2){
+    double squareSum = 0;
+
+    for(int i = 0; i < v1.size(); ++i){
+        squareSum += pow((v2.at(i) - v1.at(i)), 2);
+    }
+
+    return (0.5) * squareSum;
 }
--- tests/utiltests.cpp
+++ tests/utiltests.cpp
@@ -26,3 +26,32 @@ TEST(Util, sigmoid2){
 TEST(Util, sigmoid3){
     ASSERT_NEAR(NNetUtil::sigmoid(5), 0.993307149075715144440638, EPSILON);
 }
+
+TEST(Util, square_error1){
+    std::vector<double> v1 = std::vector<double>();
+    std::vector<double> v2 = std::vector<double>();
+    ASSERT_NEAR(NNetUtil::squareError(v1, v2), 0, EPSILON);
+}
+
+TEST(Util, square_error2){
+    std::vector<double> v1 = std::vector<double>(3, 1.0);
+    std::vector<double> v2 = std::vector<double>(3, -1.0);
+    ASSERT_NEAR(NNetUtil::squareError(v1, v2), 6.0, EPSILON);
+}
+
+TEST(Util, square_error3){
+    std::vector<double> v1 = std::vector<double>();
+    v1.push_back(1.0);
+    v1.push_back(-2.0);
+    v1.push_back(4.0);
+    v1.push_back(8.5);
+
+    std::vector<double> v2 = std::vector<double>();
+    v2.push_back(2.3);
+    v2.push_back(-3.0);
+    v2.push_back(5.0);
+    v2.push_back(-1.0);
+
+
+    ASSERT_NEAR(NNetUtil::squareError(v1, v2), 46.97, EPSILON);
+}
